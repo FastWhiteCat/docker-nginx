32bc58091b080dcdc0451af965fe83e0b9b408dc
Maciej <maciej.wieczorek@fastwhitecat.com>
2017-06-05 21:57:57 +0200

    Start fix

--- start.sh
+++ start.sh
@@ -12,13 +12,15 @@ if [ -z "$MAGE_MODE" ]; then
     MAGE_MODE="developer"
 fi
 
-if [ -z "$MAGE_MODE" ]; then
+if [ -z "$MAGE_ROOT" ]; then
     MAGE_ROOT="/var/www/html"
 fi
 
-sed -i "s/__server_app__/$SERVER_APP/g" /etc/nginx/conf.d/default.conf
-sed -i "s/__server_port__/$SERVER_PORT/g" /etc/nginx/conf.d/default.conf
-sed -i "s/__mage_mode__/$MAGE_MODE/g" /etc/nginx/conf.d/default.conf
-sed -i "s/__mage_root__/$MAGE_ROOT/g" /etc/nginx/conf.d/default.conf
+sed -i 's=__server_app__='"$SERVER_APP"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__server_port__='"$SERVER_PORT"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__mage_mode__='"$MAGE_MODE"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__mage_root__='"$MAGE_ROOT"'=g' /etc/nginx/conf.d/default.conf
+
+cat /etc/nginx/conf.d/default.conf
 
 nginx
