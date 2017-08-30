8ac98427c086bfe4dab29694788cb45dc1adee8b
Łukasz Olszewski <asdluki@gmail.com>
2017-08-30 11:11:56 +0200

    var PROTOCOL for set ht protocol

--- start.sh
+++ start.sh
@@ -20,10 +20,19 @@ if [ -z "$ALLOW_ORIGIN" ]; then
     ALLOW_ORIGIN="*"
 fi
 
+if [ -z "$PROTOCOL"]; then 
+    if [ "$MAGE_MODE" = "production" ]; then
+        PROTOCOL="HTTPS";
+    else 
+        PROTOCOL="HTTP";
+    fi
+fi
+
 sed -i 's=__server_app__='"$SERVER_APP"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__server_port__='"$SERVER_PORT"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__mage_mode__='"$MAGE_MODE"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__mage_root__='"$MAGE_ROOT"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__allow_origin__='"$ALLOW_ORIGIN"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__protocol__='"$PROTOCOL"'=g' /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
