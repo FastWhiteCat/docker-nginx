4bc920b0c607f8d5c87c6adc9ddded33e86c797f
Maciej Wieczorek <maciej.wieczorek@fastwhitecat.com>
2018-11-05 11:46:53 +0100

    Add params NGINX_KEEPALIVE_TIMEOUT & NGINX_WORKER_CONNECTIONS

--- start.sh
+++ start.sh
@@ -32,6 +32,14 @@ if [ -z "$PROTOCOL" ]; then
     fi
 fi
 
+if [ -z "$NGINX_KEEPALIVE_TIMEOUT" ]; then
+    NGINX_KEEPALIVE_TIMEOUT="65"
+fi
+
+if [ -z "$NGINX_WORKER_CONNECTIONS" ]; then
+    NGINX_WORKER_CONNECTIONS="1024"
+fi
+
 sed -i 's=__server_app__='"$SERVER_APP"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__server_port__='"$SERVER_PORT"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__mage_mode__='"$MAGE_MODE"'=g' /etc/nginx/conf.d/default.conf
@@ -39,5 +47,7 @@ sed -i 's=__mage_root__='"$MAGE_ROOT"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__allow_origin__='"$ALLOW_ORIGIN"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__client_max_body_size__='"$CLIENT_MAX_BODY_SIZE"'=g' /etc/nginx/conf.d/default.conf
 sed -i 's=__protocol__='"$PROTOCOL"'=g' /etc/nginx/conf.d/default.conf
+
+sed -i 's=__nginx_worker_connections__='"$NGINX_WORKER_CONNECTIONS"'=g' /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
